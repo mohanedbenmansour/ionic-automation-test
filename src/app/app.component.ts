24a1146620325ccde3fc2326090f7458a27dccea
mohanedbenmansour <mohaned.benmansour@esprit.tn>
2021-07-06 20:39:17 +0100

    adding dynamic splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router, private platform: Platform) {
+    this.initializeApp()
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.router.navigateByUrl('splash');
+    });
+  }
+
 }
